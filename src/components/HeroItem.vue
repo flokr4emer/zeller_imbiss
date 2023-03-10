067a791fc57f20594312f4a2c5a077818514bf14
Florian Krämer <floriankraemer1991@gmail.com>
2023-03-10 11:25:17 +0100

    Implements heros

---
+++ src/components/HeroItem.vue
@@ -0,0 +1,36 @@
+<script setup>
+//
+</script >
+
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <img class="img-fluid" :src="src" :alt="alt">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeroItem",
+    props: {
+        src: {
+            type: Object,
+            required: true,
+        },
+        alt: {
+            type: String,
+            default: "Picture"
+        }
+    },
+}
+</script>
+
+<style scoped>
+.container {
+    margin: 0;
+    padding: 0;
+}
+</style>
