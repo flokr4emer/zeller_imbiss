c52d934436342acec175d4eb2866f3d1075316b5
Florian Krämer <floriankraemer1991@gmail.com>
2023-03-10 11:28:41 +0100

    Implements scroll up button without functionality

---
+++ src/components/TheScrollUpBtn.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="btn">^</button>
+</template>
+
+<script>
+export default {
+    name: "TheScrollUpBtn",
+}
+</script>
+
+<style scoped>
+.btn {
+    background-color: brown;
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+    border: none;
+    outline: none;
+    color: white;
+    padding: 15px 23px 15px 23px;
+    border-radius: 10px;
+    font-size: 18px;
+}
+
+.btn:hover {
+    background-color: brown;
+    color: white;
+}
+</style>
